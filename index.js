68d5e7135e82675217ceb001503098c271b1d2e6
aaronms1 <amsanon1@protonmail.com>
2022-05-08 12:23:39 -0400

    added css

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+
 const app = express()
+
 const apikey = ''
 
 app.use(express.static('public'))
-
 app.use(bodyParser.urlencoded({extended:true}))
-
 app.set("veiw engine", 'ejs')
 
-app.get('/', function (req, res) {
+app.getMaxListeners('/', function (req, res) {
     res.render('index', {weather: null, error: null})
 })
 
 app.listen(3000, function () {
-    console.log("Our simple nodejs weather app is running on port 3000!");
+    console.log("Our simple nodejs express weather app is running on port 3000!");
 });
+
+
+
